54f4f34b3adef3248f75581c0f481dba45839443
Ahmad Anan Rafsanjani <appdev.ananrafs1@gmail.com>
2024-01-01 10:16:15 +0700

    refactor some

--- main.go
+++ main.go
@@ -167,86 +167,14 @@ func Exec() {
 			log.Fatalf(err.Error())
 		}
 
-		paramFormGroup := []*huh.Group{}
-		formGroup := []*huh.Group{}
+		action, params := GenerateParameter(pluginSelected.GetActionMetadata())
 
-		var (
-			selectedAction = new(string)
-			paramsOnAction = map[string]map[string]*string{}
-		)
-
-		actionSelectList := []huh.Option[string]{}
-		for k, _params := range pluginSelected.GetActionMetadata() {
-			currentForm := k
-			actionSelectList = append(actionSelectList,
-				huh.NewOption(k, k),
-			)
-			paramForm := []huh.Field{}
-			for val, q := range _params {
-				if _, ok := paramsOnAction[k]; !ok {
-					paramsOnAction[k] = map[string]*string{}
-				}
-
-				if _, ok := paramsOnAction[k][val]; !ok {
-					paramsOnAction[k][val] = new(string)
-				}
-
-				paramForm = append(paramForm,
-					huh.NewInput().
-						Title(q).
-						Value(paramsOnAction[k][val]).
-						Description((fmt.Sprintf("[%s]", currentForm))),
-				)
-			}
-
-			paramFormGroup = append(paramFormGroup,
-				huh.NewGroup(
-					paramForm...,
-				).WithHideFunc(func() bool {
-					return currentForm != *selectedAction
-				}))
-		}
-
-		actionSelectList = append(actionSelectList,
-			huh.NewOption[string]("Quit", "quit"),
-		)
-
-		formGroup = append(formGroup,
-			huh.NewGroup(
-				huh.NewSelect[string]().
-					Title("Select Action to Execute").
-					Options(actionSelectList...).
-					Value(selectedAction),
-			),
-		)
-		formGroup = append(formGroup,
-			paramFormGroup...,
-		)
-
-		form := huh.NewForm(
-			formGroup...,
-		)
-
-		err = form.Run()
-		if err != nil {
-			log.Fatalf("ERROR %s", err)
-		}
-
-		__param := map[string]string{}
-		for k, val := range paramsOnAction {
-			if k == *selectedAction {
-				for param_key, param_val := range val {
-					__param[param_key] = *param_val
-				}
-			}
-		}
-
-		if *selectedAction == "quit" {
+		if action == "quit" {
 			Exec()
 			return
 		}
 
-		msg := pluginSelected.Execute(*selectedAction, __param)
+		msg := pluginSelected.Execute(action, params)
 
 		spinner.New().Title("Execute ...").Action(func() {
 			for m := range msg {
@@ -304,4 +232,82 @@ func SelectPlugin() (plugin.Executor, error) {
 	}
 
 	return pluginSelected, nil
+}
+
+func GenerateParameter(actionParamMap map[string]map[string]string) (string, map[string]string) {
+	paramFormGroup := []*huh.Group{}
+	formGroup := []*huh.Group{}
+
+	var (
+		selectedAction = new(string)
+		paramsOnAction = map[string]map[string]*string{}
+	)
+
+	actionSelectList := []huh.Option[string]{}
+	for k, _params := range actionParamMap {
+		currentForm := k
+		actionSelectList = append(actionSelectList,
+			huh.NewOption(k, k),
+		)
+		paramForm := []huh.Field{}
+		for val, q := range _params {
+			if _, ok := paramsOnAction[k]; !ok {
+				paramsOnAction[k] = map[string]*string{}
+			}
+
+			if _, ok := paramsOnAction[k][val]; !ok {
+				paramsOnAction[k][val] = new(string)
+			}
+
+			paramForm = append(paramForm,
+				huh.NewInput().
+					Title(q).
+					Value(paramsOnAction[k][val]).
+					Description((fmt.Sprintf("[%s]", currentForm))),
+			)
+		}
+
+		paramFormGroup = append(paramFormGroup,
+			huh.NewGroup(
+				paramForm...,
+			).WithHideFunc(func() bool {
+				return currentForm != *selectedAction
+			}))
+	}
+
+	actionSelectList = append(actionSelectList,
+		huh.NewOption[string]("Quit", "quit"),
+	)
+
+	formGroup = append(formGroup,
+		huh.NewGroup(
+			huh.NewSelect[string]().
+				Title("Select Action to Execute").
+				Options(actionSelectList...).
+				Value(selectedAction),
+		),
+	)
+	formGroup = append(formGroup,
+		paramFormGroup...,
+	)
+
+	form := huh.NewForm(
+		formGroup...,
+	)
+
+	err := form.Run()
+	if err != nil {
+		log.Fatalf("ERROR %s", err)
+	}
+
+	__param := map[string]string{}
+	for k, val := range paramsOnAction {
+		if k == *selectedAction {
+			for param_key, param_val := range val {
+				__param[param_key] = *param_val
+			}
+		}
+	}
+
+	return *selectedAction, __param
 }
